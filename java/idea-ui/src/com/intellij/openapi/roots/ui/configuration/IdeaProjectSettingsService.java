2fd6821310bed531eb4a9a14b5ceccdddc3ba9dd
Tor Norbye <tnorbye@google.com>
2013-05-29 13:33:54 -0700

    Temporarily warn about gradle project settings
    Do not merge into IntelliJ! Temporary hack only.
    
    Change-Id: I6d00d25caf694e1ca487473b092a36b70e36443a

--- java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/IdeaProjectSettingsService.java
+++ java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/IdeaProjectSettingsService.java
@@ -16,6 +16,7 @@
 package com.intellij.openapi.roots.ui.configuration;
 
 import com.intellij.compiler.actions.ArtifactAwareProjectSettingsService;
+import com.intellij.ide.actions.ShowStructureSettingsAction;
 import com.intellij.ide.projectView.impl.ModuleGroup;
 import com.intellij.ide.util.projectWizard.JdkChooserPanel;
 import com.intellij.openapi.module.Module;
@@ -65,6 +66,7 @@ public class IdeaProjectSettingsService extends ProjectSettingsService implement
 
   @Override
   public void openLibrary(@NotNull final Library library) {
+    warnIfGradleProject();
     final ProjectStructureConfigurable config = ProjectStructureConfigurable.getInstance(myProject);
     ShowSettingsUtil.getInstance().editConfigurable(myProject, config, new Runnable() {
       @Override
@@ -81,6 +83,7 @@ public class IdeaProjectSettingsService extends ProjectSettingsService implement
 
   @Override
   public void openModuleSettings(final Module module) {
+    warnIfGradleProject();
     ModulesConfigurator.showDialog(myProject, module.getName(), null);
   }
 
@@ -91,6 +94,7 @@ public class IdeaProjectSettingsService extends ProjectSettingsService implement
 
   @Override
   public void openModuleLibrarySettings(final Module module) {
+    warnIfGradleProject();
     ModulesConfigurator.showDialog(myProject, module.getName(), ClasspathEditor.NAME);
   }
 
@@ -101,6 +105,7 @@ public class IdeaProjectSettingsService extends ProjectSettingsService implement
 
   @Override
   public void openContentEntriesSettings(final Module module) {
+    warnIfGradleProject();
     ModulesConfigurator.showDialog(myProject, module.getName(), ContentEntriesEditor.NAME);
   }
 
@@ -111,6 +116,7 @@ public class IdeaProjectSettingsService extends ProjectSettingsService implement
 
   @Override
   public void openModuleDependenciesSettings(@NotNull final Module module, @Nullable final OrderEntry orderEntry) {
+    warnIfGradleProject();
     ShowSettingsUtil.getInstance().editConfigurable(myProject, ProjectStructureConfigurable.getInstance(myProject), new Runnable() {
       @Override
       public void run() {
@@ -126,6 +132,7 @@ public class IdeaProjectSettingsService extends ProjectSettingsService implement
 
   @Override
   public void openLibraryOrSdkSettings(@NotNull final OrderEntry orderEntry) {
+    warnIfGradleProject();
     final ProjectStructureConfigurable config = ProjectStructureConfigurable.getInstance(myProject);
     ShowSettingsUtil.getInstance().editConfigurable(myProject, config, new Runnable() {
       @Override
@@ -166,6 +173,14 @@ public class IdeaProjectSettingsService extends ProjectSettingsService implement
 
   @Override
   public void openArtifactSettings(@Nullable Artifact artifact) {
+    warnIfGradleProject();
     ModulesConfigurator.showArtifactSettings(myProject, artifact);
+  }
+
+  // TEMPORARY HACK!! REMOVE ME ASAP!
+  private void warnIfGradleProject() {
+    if (ShowStructureSettingsAction.isGradleProject(myProject)) {
+      ShowStructureSettingsAction.showDisabledProjectStructureDialogMessage();
+    }
   }
 }
